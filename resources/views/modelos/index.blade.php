ed601757005bc69b783cead3ffa8e7e48c1bd47d
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-06 22:37:12 -0300

    ADD table to rendering data

--- resources/views/modelos/index.blade.php
+++ resources/views/modelos/index.blade.php
@@ -3,9 +3,28 @@
 @section('content')
 <div class="container">
     <div class="col-md-12">
-        @foreach($modelos as $modelo)
-            <h3>Modelo: {{ $modelo->modelo }}</h3>
-        @endforeach
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Modelo</th>
+                <th scope="col">Funções</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($modelos as $modelo)
+                    <tr>
+                        <th scope="row">{{ $modelo->id }}</th>
+                        <td> {{ $modelo->modelo }} </td>
+                        <td>
+                            <a href="{{ route('modelos.show', ['modelo' => modelo->id]) }}">Visualizar</a>
+                            <a href="#">Editar</a>
+                            <a href="#">Excluir</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
